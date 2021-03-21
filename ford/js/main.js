40631297a4aa5f80db905e3fc7bbd88494de3b97
Debugg <debuggforbussiness@gmail.com>
2021-03-21 17:09:47 +0100

    first push

---
+++ ford/js/main.js
@@ -0,0 +1,17 @@
+// Stoppa användare i firefox från att dra bilder till en ny flik
+$(document).on("dragstart", function(e) {
+  if (e.target.nodeName.toUpperCase() == "IMG") {
+    return false;
+  }
+});
+
+$(document).ready( function() {
+  $('[data-toggle="navvers"]').click( function() {
+    $(this).toggleClass('open');
+    if ($(this).hasClass('open')) {
+      $("#menu-mobile").slideDown( "slow" );
+    } else {
+      $("#menu-mobile").fadeOut();
+    }
+  });
+});
